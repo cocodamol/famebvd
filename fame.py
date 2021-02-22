806d779ba5439ec9920e50bdc66151f64fd453b7
cocodamol <32512538+cocodamol@users.noreply.github.com>
2021-02-22 10:48:03 +0000

    Update fame.py
    use selenium to create bulk requests in fame's web ui. kinda missing a step downloading files from the export page but you can easily do that in web ui once the requests are fulfilled.

--- fame.py
+++ fame.py
@@ -1,3 +1,4 @@
+
 from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
@@ -17,12 +18,11 @@ def login(username, password):
     driver.get('https://libguides.kcl.ac.uk/az.php?a=f')
     driver.find_element_by_xpath('//*[@id="s-lg-az-name-f"]/div[5]/div[1]/a').click()
     driver.switch_to.window(driver.window_handles[1])
-
     driver.find_element_by_xpath('//*[@id="organisation-select-view"]/p[2]/a').click()
-    driver.find_element_by_xpath('// *[ @ id = "combobox"]/ option[1123]').click()
+    driver.find_element_by_xpath('//*[@id="combobox"]/option[1126]').click()
     time.sleep(1)
     driver.find_element_by_xpath(' // *[ @ id = "submit-btn"]').click()
-    usernameBox = WebDriverWait(driver, 3).until(EC.visibility_of_element_located
+    usernameBox = WebDriverWait(driver, 10).until(EC.visibility_of_element_located
                                                   ((By.XPATH, '//*[@id="i0116"]')))
     usernameBox.send_keys(username)
     driver.refresh()
@@ -40,30 +40,34 @@ def login(username, password):
     Next = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.ID, 'idBtn_Back')))
     Next.click()
     time.sleep(8)
-    # go to default set results view
-    # driver.get("https://fame4.bvdinfo.com/version-20201112/fame/1/Companies/List/Display/6e88ea9f-7831-eb11-9009-00155d115b03");
+    # set your view in web ui first, apply filters, select years etc, and then save as set. 
+    # so next time you login you'd be taken go to default set view. clicking around w selenium does not work well
     driver.get("https://fame4.bvdinfo.com/version-20201112/fame/1/Companies/List")
     driver.implicitly_wait(10)
 
 def export_set(start, end):
-    # while start < 7778452:
-    while start < 266420:
-        # click 'excel'
+    while start < 7785859:
+            # click 'excel'
         excel = WebDriverWait(driver, 15).until(
             EC.element_to_be_clickable((By.XPATH, '/html/body/section[3]/div[1]/div[2]/div[2]/div[1]/ul/li[3]')))
         excel.click()
         time.sleep(2)
         # choose range
-        options = WebDriverWait(driver, 2).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="component_RangeOptionSelectedId"]/option[4]')))
-        options.click()
+        # driver.find_element_by_xpath('//*[@id="component_RangeOptionSelectedId"]/option[4]').click()
+        try:
+            range_select = WebDriverWait(driver, 30).until(
+                EC.visibility_of_element_located((By.XPATH, '//*[@id="component_RangeOptionSelectedId"]/option[4]')))
+            range_select.click()
+        except:
+            range_select = WebDriverWait(driver, 30).until(
+                EC.visibility_of_element_located((By.XPATH, '//*[@id="component_RangeOptionSelectedId"]/option[4]')))
+            range_select.click()
 
-
-        file_name = WebDriverWait(driver, 2).until(
+        file_name = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.NAME, 'component.FileName')))
-        from_box = WebDriverWait(driver, 2).until(
+        from_box = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.NAME, 'component.From')))
-        to_box = WebDriverWait(driver, 2).until(
+        to_box = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.NAME, 'component.To')))
         file_name.clear()
         file_name.send_keys('unfiltered_{}-{}'.format(start,end))
@@ -76,48 +80,40 @@ def export_set(start, end):
         submit.click()
         time.sleep(1)
         # close down download popup
-        close = WebDriverWait(driver, 1).until(
+        close = WebDriverWait(driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, '/ html / body / section[3] / div[6] / div[1] / img')))
         close.click()
-
         print(start, end)
         start, end = range(start, end)
         time.sleep(5)
 
 
 def range(start,end):
-#check web interface and get max numof row allowed to be exported 
     start = start +1730
     end = end + 1730
     return start, end
 
 
 if __name__ == "__main__":
-    chrome_options = Options()
-    chrome_options.add_experimental_option("prefs", {
-        "download.default_directory": r"D:\fame\unfiltered",
+    options = Options()
+    options.add_experimental_option("prefs", {
+        "download.default_directory": r"C:\Users\jiangx\PycharmProjects\famebvd\famebvd\unfiltered",
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True,
         "profile.default_content_setting_values.automatic_downloads": 1,
     })
-    driver = webdriver.Chrome("D:/PyCharm projects/famebvd/chromedriver.exe", chrome_options=chrome_options)
+    #  where's your driver
+    driver = webdriver.Chrome("C:/Users/jrjrjrjr/PycharmProjects/famebvd/famebvd/chromedriver.exe", options=options)
 
 
-    username = "ssssss"
+    username = ""
     # replace with username
-    password = "sssssss"
+    password = ""
     # replace with password
     start_url= 'https://libguides.kcl.ac.uk/az.php?a=f'
-    start = 1
-    end = 262962
+    start =1
+    end = start + 1729
 
     login(username, password)
     export_set(start, end)
-
-
-
-
-
-
-
